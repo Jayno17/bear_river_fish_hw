8aa71633fd2f82e0c1b9836469f0918d2a2c2723
Jayno17 <julia.nowak@gmail.com>
2020-01-21 22:07:51 +0000

    added bear type

--- bear.rb
+++ bear.rb
@@ -1,10 +1,15 @@
 class Bear
 
-  def initialize(name)
+  def initialize(name, type)
     @name = name
+    @type = type
   end
 
   def bear_name()
     return @name
+  end
+
+  def bear_type()
+    return @type
   end
 end
--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -6,9 +6,13 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 class BearTest < MiniTest::Test
 
   def setup()
-  @bear = Bear.new("Yogi")
+  @bear = Bear.new("Yogi","brown")
   end
+
   def test_bear_name()
     assert_equal("Yogi", @bear.bear_name)
+ end
+def test_bear_type()
+   assert_equal("brown", @bear.bear_type)
  end
  end
